6d2256b9f88a0c8269a5588966eeaceed57edbf9
Lafoerte91 <lafoerte@gmail.com>
2024-10-20 20:52:06 +0300

    иммутабельность

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,49 @@
-import './app.css'
-import AppInfo from '../app-info/app-info'
-import SearchPanel from '../search-panel/search-panel'
-import AppFilter from '../app-filter/app-filter'
-import EmployersList from '../employers-list/employers-list'
-import EmpoyersAddForm from '../employers-add-form/employers-add-form'
+import { Component } from 'react';
+import './app.css';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 
-function App() {
-  const data = [
-    {name: 'John C.', salary: 800, increase: false, id: 1},
-    {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-    {name: 'Carl W.', salary: 15000, increase: false, id: 3},
-  ]
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+        { name: 'Carl W.', salary: 15000, increase: false, id: 3 },
+      ],
+    };
+  }
 
-  return(
-    <div className="app">
-      <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
-      <div className="search-panel">
-        <SearchPanel />
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
 
-        <AppFilter />
+        <div className="search-panel">
+          <SearchPanel />
+
+          <AppFilter />
+        </div>
+
+        <EmployersList data={this.state.data} onDelete={this.deleteItem} />
+
+        <EmployersAddForm />
       </div>
-
-      <EmployersList data={data} />
-
-      <EmpoyersAddForm />
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
+
